acc117a047d9312a8eefb993dd8110b280f8a09e
Etienne Colmant <colmant.etienne@gmail.com>
2024-06-20 14:41:21 +0200

    Dernière modif du login

--- Backend/login.js
+++ Backend/login.js
@@ -1,97 +1,86 @@
-const identifiant = {
-    email: "sophie.bluel@test.tld",
-    password: "S0phie",
-}
+const emailLogin = document.getElementById("emailLogin")
+const passWordLogin = document.getElementById("passWord")
+const btnConnection = document.getElementById("connection")
 
-const chargeUtile = JSON.stringify(identifiant);
+const divMessage = document.querySelector(".message")
 
-fetch("http://localhost:5678/api/users/login", {
-    method: "POST",
-    headers: {
-        'Accept': 'application/json',
-        "Content-Type": "application/json;charset=utf-8",
-    },
-    body: chargeUtile,
-})
-.then(response => response.json())
-.then(data => {
+btnConnection.addEventListener("click", (event) =>{
+    event.preventDefault()
 
-    function verifierEmail(balise){
-        let emailRegExp = new RegExp("[a-zA-Z._-]+@[a-zA-Z._-]+\\.[a-z]+")
-    
-        if (emailRegExp.test(balise.value) && balise.value === identifiant.email){
-            balise.style.boxShadow = "0px 4px 14px rgba(0, 0, 0, 0.09)"
-        } else {
-            balise.style.boxShadow = "0px 4px 14px rgba(255, 0, 0, 0.5)"
-        }
-    }
-    
-    function verifierPassWord(balise){
-    
-        if(balise.value === identifiant.password){
-            balise.style.boxShadow = "0px 4px 14px rgba(0, 0, 0, 0.09)"
-        } else {
-            balise.style.boxShadow = "0px 4px 14px rgba(255, 0, 0, 0.5)"
-        }
+    const identifiant = {
+        email: emailLogin.value,
+        password: passWordLogin.value,
     }
 
-    function clickconnection(){
-    
-        const emailLogin = document.getElementById("emailLogin")
-        const passWordLogin = document.getElementById("passWord")
-        const btnConnection = document.getElementById("connection")
+    const chargeUtile = JSON.stringify(identifiant);
 
-        const divMessage = document.querySelector(".message")
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            "Content-Type": "application/json;charset=utf-8",
+        },
+        body: chargeUtile,
+    })
+    .then(response => response.json())
+    .then(data => {
 
-    
-        btnConnection.addEventListener("click", (event) =>{
-            event.preventDefault()
+        function verifierEmail(balise){
+            let emailRegExp = new RegExp("[a-zA-Z._-]+@[a-zA-Z._-]+\\.[a-z]+")
+        
+            if (emailRegExp.test(balise.value)){
+                balise.style.boxShadow = "0px 4px 14px rgba(0, 0, 0, 0.09)"
+            } else {
+                balise.style.boxShadow = "0px 4px 14px rgba(255, 0, 0, 0.5)"
+            }
+        }
+        
+        function verifierPassWord(balise){
+        
+            if(balise.value === ""){
+                balise.style.boxShadow = "0px 4px 14px rgba(255, 0, 0, 0.5)"
+            } else {
+                balise.style.boxShadow = "0px 4px 14px rgba(0, 0, 0, 0.09)"
+            }
+        }
 
-            if (emailLogin.value === identifiant.email && passWordLogin.value === identifiant.password){
+        verifierEmail(emailLogin)
+        
+        verifierPassWord(passWordLogin)
+        
+        if (data.token){
 
-                verifierEmail(emailLogin)
-            
-                verifierPassWord(passWordLogin)
+            const token = data.token
+            const valeurToken = JSON.stringify(token)
 
-                const token = data.token
-                const valeurToken = JSON.stringify(token)
+            window.localStorage.setItem("token", valeurToken)
 
-                window.localStorage.setItem("token", valeurToken)
+            document.location.href = "index.html"
+        
+        } else {
 
-                document.location.href="index.html"
-            
+            if(divMessage.style.display === "flex"){
+
+                divMessage.innerHTML = ""
+
+                divMessage.style.display = "flex"
+                const textElement = document.createElement("p")
+                textElement.innerText = "Erreur dans l'identifiant ou le mot de passe"
+
+                divMessage.appendChild(textElement)
+
             } else {
 
-                verifierEmail(emailLogin)
-            
-                verifierPassWord(passWordLogin)
+                divMessage.style.display = "flex"
+                const textElement = document.createElement("p")
+                textElement.innerText = "Erreur dans l'identifiant ou le mot de passe"
 
-                if(divMessage.style.display === "flex"){
-
-                    divMessage.innerHTML = ""
-
-                    divMessage.style.display = "flex"
-                    const textElement = document.createElement("p")
-                    textElement.innerText = "Erreur dans l'identifiant ou le mot de passe"
-    
-                    divMessage.appendChild(textElement)
-
-                } else {
-
-                    divMessage.style.display = "flex"
-                    const textElement = document.createElement("p")
-                    textElement.innerText = "Erreur dans l'identifiant ou le mot de passe"
-    
-                    divMessage.appendChild(textElement)
-                }
+                divMessage.appendChild(textElement)
             }
-        })
-    }
-      
-    clickconnection()
-    
+        }
+    })
+    .catch(error => {
+        console.error('Erreur:', error);
+    });
 })
-.catch(error => {
-    console.error('Erreur:', error);
-});
 
